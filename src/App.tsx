dcda1b3c9e33a382d9f5d764e64a41073215f1d8
wolfgunblood <jalajdorai1@gmail.com>
2023-06-04 17:44:06 +0530

    Added Jobs Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./App.css";
 import Header from "../src/assets/images/bg-header-desktop.svg";
+import JobsContainer from './components/JobsContainer/JobsContainer';
 
 const App = () => {
   return (
     <div className='App'>
       <img src={Header} alt="header" className='headerImage' />
+      <JobsContainer />
     </div>
   )
 }
