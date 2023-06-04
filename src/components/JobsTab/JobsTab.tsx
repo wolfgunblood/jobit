54b1532eee7fda3d76287d73079410162d61f38b
wolfgunblood <jalajdorai1@gmail.com>
2023-06-04 19:00:50 +0530

    Updated Code now creating Jobs tab

--- src/components/JobsTab/JobsTab.tsx
+++ src/components/JobsTab/JobsTab.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-// import CompanyLogo from "./images/loop-studios.svg"
+import './JobsTab.scss';
 
-const JobsTab = ({item,index}) => {
+const JobsTab = ({ item, index }) => {
 
-  const {logo,company} = item;
+  const { logo, company, contract, tools, languages, role, position, level } = item;
   // console.log(logo)
   // console.log(CompanyLogo)
   return (
     <div className='job-card'>
-      <div>
-        <img src={logo} alt="Company Logo" className='company-logo'/>
+      <div className='company-info-container'>
+        <div className='company-logo-container'>
+          <img src={logo} alt="Company Logo" className='company-logo' />
+        </div>
         <div className='company-info'>
-          <h3 className='company-name'>{company}</h3>
+          <p className='company-name'>{company}</p>
+          <h4 className='position-name'>{position}</h4>
         </div>
       </div>
+
+      <div className='btns-container'>
+        <button className='btn'>{role}</button>
+        <button className='btn'>{level}</button>
+        {tools.map((tool, index) => (
+          <button key={index} className='btn'>{tool}</button>
+        ))}
+        {languages.map((language, index) => (
+          <button key={index} className='btn'>{language}</button>
+        ))}
+      </div>
+
     </div>
   )
 }
